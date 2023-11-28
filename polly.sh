d101d9a2084cdce1f4469b2db3b9308785028b47
matttMP <66797927+matttMP@users.noreply.github.com>
2023-11-27 23:00:42 -0600

    Create polly.sh

---
+++ polly.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Run this script as root (sudo su)"
+    exit
+fi
+
+echo "+##############################################+"
+echo "# Welcome To Pollen!                           #"
+echo "# The User Policy Editor                       #"
+echo "# -------------------------------------------- #"
+echo "# Developers:                                  #"
+echo "# - OlyB                                       #"
+echo "# - Rafflesia                                  #"
+echo "# - Scaratek                                   #"
+echo "# - r58Playz                                   #"
+echo "+##############################################+"
+echo "May Ultrablue Rest in Peace, o7"
+
+sleep 1
+
+mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
+echo '{"WallpaperImage":[], "URLBlocklist": [], "SystemFeaturesDisableList": [], "EditBookmarksEnabled": true, "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "DefaultPopupsSetting": 1, "AllowDeletingBrowserHistory": true, "AllowDinosaurEasterEgg": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
+mount --bind /tmp/overlay/etc /etc
+
+echo ""
+echo "Pollen has been successfully applied!"
